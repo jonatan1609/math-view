c590e3fcefed7e36c3ba9410b5b6cf451ae5bee1
Jonatan <5377074j@gmail.com>
2020-06-15 00:48:44 +0300

    add picture

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -9,4 +9,6 @@ def main_page():
     return render_template('index.html')
 
 
+app.add_url_rule('/favicon.ico',
+                 redirect_to=url_for('static', filename='favicon.ico'))
 app.run()
