a0cd9615c72d903c41683dcc89a49179997a1e59
jonatan1609 <49692607+jonatan1609@users.noreply.github.com>
2020-06-15 13:42:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ def main_page():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
+   return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
                                'favicon.ico', mimetype='image/png')
 
 
-app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
