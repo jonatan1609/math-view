bd7c0d45f317ac5eb16067cd911a82dff5354a0d
jonatan1609 <49692607+jonatan1609@users.noreply.github.com>
2020-06-15 13:36:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main_page():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'favicon.ico', mimetype='image/png')
 
 
 app.run()
