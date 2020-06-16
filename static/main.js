99723ab1b4f0c2d3072c24c411488e6a73fddcc2
Jonatan <5377074j@gmail.com>
2020-06-16 09:39:33 +0300

    Version 1.0.0

--- static/main.js
+++ static/main.js
@@ -1,9 +1,10 @@
+var box = document.getElementById('inputs')
+var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var charactersLength = characters.length;
 
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var charactersLength = characters.length;
 function makeId(length) {
-   for ( var i = 0; i < length; i++ ) {
+    var result           = '';
+    for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
    }
    return result;
@@ -11,19 +12,18 @@ function makeId(length) {
 
 function addRow(){
 var div = document.createElement('div');
-var box = document.getElementById('inputs')
-var line = document.createElement('br');
+var atag = document.createElement('a');
+var image = document.createElement('img');
 var element = document.createElement('input');
 var view = document.createElement('p');
-var atag = document.createElement('a')
-var image = document.createElement('img');
-image.src = "/static/minus-ico.png";
-image.alt = "remove line"
-element.id = makeId(20);
-div.id = element.id
+var line = document.createElement('br');
+div.id = element.id;
 atag.href = `javascript:removeLine('${element.id}')`;
-image.style = "width:30px;top:10px;display: inline-block;position:relative"
 atag.appendChild(image);
+image.src = "/static/minus-ico.png";
+image.alt = "remove line";
+image.style = "width:30px;top:10px;display: inline-block;position:relative";
+element.id = makeId(20);
 element.oninput = onInput;
 view.style = "display: inline-block;margin-left:30px";
 view.id = element.id;
@@ -35,28 +35,36 @@ box.appendChild(div);
 }
 
 function onInput(){
-velem = document.querySelector(`p[id="${this.id}"]`)
-text = encodeURIComponent(this.value);
-var request = new XMLHttpRequest();
-request.open('GET', `/get?val=${text}`);
-request.onreadystatechange = (e) => {if(request.status == 200){velem.innerHTML = request.responseText}}
-request.send()
+    var request = new XMLHttpRequest();
+    velem = document.querySelector(`p[id="${this.id}"]`)
+    text = encodeURIComponent(this.value);
+    request.open('GET', `/get?val=${text}`);
+    request.onreadystatechange = (e) => {
+        if (request.status == 200){
+            velem.innerHTML = request.responseText;
+        }
+    }
+    request.send()
 }
-function removeLine(n){
 
+function removeLine(n){
 let x = document.getElementById('inputs');
-for(node of x.childNodes){
-if(node.id == n){x.removeChild(node)}
-}}
+    for (node of x.childNodes){
+        if (node.id == n){
+            x.removeChild(node)
+        }
+    }
+}
+
 function click1(){
-o = document.getElementById("b");
-p = document.getElementById("p");
-if (o.className.includes("active")){
-  o.className = "btn1";
-   p.innerText = "Mode 1";
-  }
- else {
- o.className += " active";
- p.innerText = "Mode 2";
- }
+    o = document.getElementById("b");
+    p = document.getElementById("p");
+    if (o.className.includes("active")){
+        o.className = "btn1";
+        p.innerText = "Mode 1";
+    }
+    else {
+        o.className += " active";
+        p.innerText = "Mode 2";
+    }
 }
